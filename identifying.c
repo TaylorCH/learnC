5e06c5d087d09edf77d5d9378d49131bfa702af0
TaylorCH <ch0691230@hotmail.com>
2018-08-21 21:41:48 +0800

    Learn two

---
+++ identifying.c
@@ -0,0 +1,15 @@
+/* Forth */ 
+# include <stdio.h>
+
+main()
+{
+	int i, j, k;
+	
+	printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+	printf("%d %d %d\n", i, j, k);
+	scanf("%d a%d /%d",&i,&j,&k);
+	printf("%d %d %d", i, j, k);
+	
+	
+	return 0;
+}
