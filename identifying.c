98b576941016bdf2747eafd2cc3aefd7cd44c1be
TaylorCH <ch0691230@hotmail.com>
2018-08-27 16:54:45 +0800

    Learn three

--- identifying.c
+++ identifying.c
@@ -4,11 +4,27 @@
 main()
 {
 	int i, j, k;
+	float ii;
+	char ch1, ch2;
 	
-	printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
-	printf("%d %d %d\n", i, j, k);
-	scanf("%d a%d /%d",&i,&j,&k);
-	printf("%d %d %d", i, j, k);
+	
+	
+	scanf("%c", &ch1);
+	printf("%d", (int) ch1);
+	
+	
+	
+//	do
+//	{
+//		scanf("%c", &ch);
+//	} while (ch != '\n');
+	
+//	printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+//	scanf("%f", &ii);
+//	printf("%f", ii);
+//	printf("%d %d %d\n", i, j, k);
+//	scanf("%d a%d /%d",&i,&j,&k);
+//	printf("%d %d %d", i, j, k);
 	
 	
 	return 0;
